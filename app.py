07b742b465eea5069f388cc10e4d052d68fa538d
Krupka-Yuliia <yuliia.krupka.22@pnu.edu.ua>
2024-10-15 18:02:33 +0300

    add greetings

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +17,13 @@ def greetings(name):
     name = name.upper()
     age = request.args.get("age", 0)
     return f"Welcome {name=} {age=}"
+
+@app.route('/admin')
+def admin():
+    to_url = url_for("greetings", name="administrator", external=True)   # --> "/hi/admin"
+    print(to_url)
+    return redirect(to_url)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
