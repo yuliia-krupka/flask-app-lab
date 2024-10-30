846b8398e1054b9c1ef980b22c231d83f6e39a84
Krupka-Yuliia <yuliia.krupka.22@pnu.edu.ua>
2024-10-30 17:21:55 +0200

    add cookie

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,6 @@
 from . import user_bp
-from flask import render_template, request, redirect, url_for, abort
+from flask import render_template, request, redirect, url_for, abort, make_response
+from datetime import datetime, timedelta
 
 
 @user_bp.route('/hi/<string:name>')     # /hi/ivan?age=30
@@ -13,3 +14,20 @@ def admin():
     to_url = url_for("users.greetings", name="administrator", age=45, _external=True)   #"http://localhost:8080/hi/administrator?age=45"
     print(to_url)
     return redirect(to_url)
+
+@user_bp.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кука встановлена')
+    response.set_cookie('username', 'student', max_age=timedelta(seconds=60))
+    return response
+
+@user_bp.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return f'Користувач: {username}'
+
+@user_bp.route('/delete_cookie')
+def delete_cookie():
+    response = make_response('Кука видалена')
+    response.set_cookie('username', '', expires=0) # response.set_cookie('username', '', max_age=0)
+    return response
